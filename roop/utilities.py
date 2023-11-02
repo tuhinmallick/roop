f5709fcd22f73e2ce742cddc63283eb3aef492a9
Sourcery AI <>
2023-11-02 14:58:51 +0000

    'Refactored by Sourcery'

--- roop/utilities.py
+++ roop/utilities.py
@@ -21,8 +21,13 @@ if platform.system().lower() == 'darwin':
 
 
 def run_ffmpeg(args: List[str]) -> bool:
-    commands = ['ffmpeg', '-hide_banner', '-loglevel', roop.globals.log_level]
-    commands.extend(args)
+    commands = [
+        'ffmpeg',
+        '-hide_banner',
+        '-loglevel',
+        roop.globals.log_level,
+        *args,
+    ]
     try:
         subprocess.check_output(commands, stderr=subprocess.STDOUT)
         return True
@@ -45,14 +50,41 @@ def detect_fps(target_path: str) -> float:
 def extract_frames(target_path: str, fps: float = 30) -> bool:
     temp_directory_path = get_temp_directory_path(target_path)
     temp_frame_quality = roop.globals.temp_frame_quality * 31 // 100
-    return run_ffmpeg(['-hwaccel', 'auto', '-i', target_path, '-q:v', str(temp_frame_quality), '-pix_fmt', 'rgb24', '-vf', 'fps=' + str(fps), os.path.join(temp_directory_path, '%04d.' + roop.globals.temp_frame_format)])
+    return run_ffmpeg(
+        [
+            '-hwaccel',
+            'auto',
+            '-i',
+            target_path,
+            '-q:v',
+            str(temp_frame_quality),
+            '-pix_fmt',
+            'rgb24',
+            '-vf',
+            f'fps={fps}',
+            os.path.join(
+                temp_directory_path, f'%04d.{roop.globals.temp_frame_format}'
+            ),
+        ]
+    )
 
 
 def create_video(target_path: str, fps: float = 30) -> bool:
     temp_output_path = get_temp_output_path(target_path)
     temp_directory_path = get_temp_directory_path(target_path)
     output_video_quality = (roop.globals.output_video_quality + 1) * 51 // 100
-    commands = ['-hwaccel', 'auto', '-r', str(fps), '-i', os.path.join(temp_directory_path, '%04d.' + roop.globals.temp_frame_format), '-c:v', roop.globals.output_video_encoder]
+    commands = [
+        '-hwaccel',
+        'auto',
+        '-r',
+        str(fps),
+        '-i',
+        os.path.join(
+            temp_directory_path, f'%04d.{roop.globals.temp_frame_format}'
+        ),
+        '-c:v',
+        roop.globals.output_video_encoder,
+    ]
     if roop.globals.output_video_encoder in ['libx264', 'libx265', 'libvpx']:
         commands.extend(['-crf', str(output_video_quality)])
     if roop.globals.output_video_encoder in ['h264_nvenc', 'hevc_nvenc']:
@@ -70,7 +102,12 @@ def restore_audio(target_path: str, output_path: str) -> None:
 
 def get_temp_frame_paths(target_path: str) -> List[str]:
     temp_directory_path = get_temp_directory_path(target_path)
-    return glob.glob((os.path.join(glob.escape(temp_directory_path), '*.' + roop.globals.temp_frame_format)))
+    return glob.glob(
+        os.path.join(
+            glob.escape(temp_directory_path),
+            f'*.{roop.globals.temp_frame_format}',
+        )
+    )
 
 
 def get_temp_directory_path(target_path: str) -> str:
@@ -89,7 +126,9 @@ def normalize_output_path(source_path: str, target_path: str, output_path: str) 
         source_name, _ = os.path.splitext(os.path.basename(source_path))
         target_name, target_extension = os.path.splitext(os.path.basename(target_path))
         if os.path.isdir(output_path):
-            return os.path.join(output_path, source_name + '-' + target_name + target_extension)
+            return os.path.join(
+                output_path, f'{source_name}-{target_name}{target_extension}'
+            )
     return output_path
 
 
